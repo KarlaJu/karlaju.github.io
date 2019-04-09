15b9c1c53f826e26a5dad210c269476d30a152a4
KarlaJu <karla@makingdevs.com>
2019-04-08 23:47:36 -0500

    :d

--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// smooth-scroll
+$.smoothScroll({
+    //滑动到的位置的偏移量
+    offset: 0,
+    //滑动的方向，可取 'top' 或 'left'
+    direction: 'top',
+    // 只有当你想重写默认行为的时候才会用到
+    scrollTarget: null,
+    // 滑动开始前的回调函数。`this` 代表正在被滚动的元素
+    beforeScroll: function () { },
+    //滑动完成后的回调函数。 `this` 代表触发滑动的元素
+    afterScroll: function () { },
+    //缓动效果
+    easing: 'swing',
+    //滑动的速度
+    speed: 700,
+    // "自动" 加速的系数
+    autoCoefficent: 2
+});
 
-},{}]},{},[1])
+
+// Bind the hashchange event listener
+$(window).bind('hashchange', function (event) {
+    $.smoothScroll({
+        // Replace '#/' with '#' to go to the correct target
+        offset: $("body").attr("data-offset")? -$("body").attr("data-offset"):0 ,
+        // offset: -30,
+        scrollTarget: decodeURI(location.hash.replace(/^\#\/?/, '#'))
+        
+      });
+});
+
+// $(".smooth-scroll").on('click', "a", function() {
+$('a[href*="#"]')
+    .bind('click', function (event) {    
+    // Remove '#' from the hash.
+    var hash = this.hash.replace(/^#/, '')
+    if (this.pathname === location.pathname && hash) {
+        event.preventDefault();
+        // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+        location.hash = '#/' + hash;
+    }
+});
+
+// Trigger hashchange event on page load if there is a hash in the URL.
+if (location.hash) {
+    $(window).trigger('hashchange');
+}
+
+// // $('[data-spy="scroll"]').each(function () {
+// //     var $spy = $(this).scrollspy('refresh')
+// //   })
+
+// $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
+//     // do something…
+//     var offset = $('[data-spy="scroll"]').attr("data-offset")
+//   })
